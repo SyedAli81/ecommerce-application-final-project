12ec1b0010ffcd5562009f55e36e0ac5f4e4bfed
SYED <syedali05125569@gmail.com>
2023-05-02 21:28:47 -0400

    updated packages and made the template
    updated packages and made the template

---
+++ src/template/ScrollToTopOnMount.js
@@ -0,0 +1,12 @@
+import {useEffect} from "react";
+
+function ScrollToTopOnMount(){
+    useEffect(() => {
+        window.scrollTo(0, 0);
+    }, []);
+
+    return null;
+}
+
+
+export default ScrollToTopOnMount
---
+++ src/template/Template.js
@@ -0,0 +1,13 @@
+import Header from "./Header"
+import Content from "./Content"
+import Footer from "./Footer"
+
+function Template(props){
+    return (
+        <>
+            <Header />
+            <Content>{props.children}</Content>
+            <Footer />
+        </>
+    )
+}
