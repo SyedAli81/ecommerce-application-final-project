73d4085c3a11e7ef75ddbac2d60db93b0962e515
JGJ <jordan.gb0@gmail.com>
2023-05-04 19:57:10 -0400

    first draft of navbar with search bar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import NavBarComp from './components/NavBarComp';
 
 function App() {
   return (
     <div className="App">
-      
+      <NavBarComp/>
     </div>
   );
 }
