87ad83d73571d318e76a801b86705bb209f4e63c
siutsang <95592154+royshtsang@users.noreply.github.com>
2023-05-05 15:25:40 -0400

    client updated

--- client/src/components/LoginForm.js
+++ client/src/components/LoginForm.js
@@ -2,7 +2,7 @@
 import React, { useState } from 'react';
 import { Form, Button, Alert } from 'react-bootstrap';
 
-import { LOGIN_USER } from '../utils/mutations'; 
+import { loginUser } from '../utils/API';
 import Auth from '../utils/auth';
 
 const LoginForm = () => {
