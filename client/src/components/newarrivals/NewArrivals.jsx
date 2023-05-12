927573d0232a12f711c7c9dbf0a93d849d259c3d
SYED <syedali05125569@gmail.com>
2023-05-12 15:40:59 -0400

    updated new Arrival section

--- client/src/components/newarrivals/NewArrivals.jsx
+++ client/src/components/newarrivals/NewArrivals.jsx
@@ -9,9 +9,15 @@ const NewArrivals = () => {
         <div className="container">
           <div className="heading d_flex">
             <div className="heading-left row f_flex">
-              <img src="https://img.icons8.com/glyph-neue/64/26e07f/new.png"
+              <img src="https://img.icons8.com/glyph-neue/64/26e07f/new.png" />
+              <h2>New Arrivals</h2>
+            </div>
+            <div className="heading-right row">
+              <span>View All</span>
+              <i className="fa-solid fa-caret-right"></i>
             </div>
           </div>
+          <Cart />
         </div>
       </section>
     </>
